8d49af307ad2209e45de0c233e586209327978d9
kotik06 <55040629+kotik06@users.noreply.github.com>
2020-04-20 12:01:13 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,11 @@
+
+pkg install wget
+pkg install php
+pkg install python
+pkg install python2
+python3 -m pip install vk
+python3 -m pip install vk_api
+python3 -m pip install colorama
+python3 -m pip install bs4
+python3 -m pip install pysocks
+python3 -m pip install prettytable
