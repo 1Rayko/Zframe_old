f0cac7f56c0f5b4c35455e101882c8ffb6c644b3
kotik06 <55040629+kotik06@users.noreply.github.com>
2020-05-03 15:42:26 +0700

    Update Zen.py

--- Z/Zen.py
+++ Z/Zen.py
@@ -84,7 +84,7 @@ while True:
     elif num == ('2'):
         print (Fore.GREEN + 'Starting Seeker')
         os.system('clear')
-	os.chdir('seeker-master')
+        os.chdir('seeker-master')
         os.system('chmod 777 template/nearyou/php/info.txt')
         os.system('chmod 777 template/nearyou/php/result.txt')
         os.system('python3 seeker.py -t manual')
